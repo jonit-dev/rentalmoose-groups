d8a3afab14f14d1c3d1ccad588b13f983621b7a1
Joao Paulo Furtado Silva <joaopaulofurtado@live.com>
2018-11-09 08:36:51 -0800

    Added general groups

--- angular/controllers/mainCtrl.js
+++ angular/controllers/mainCtrl.js
@@ -8,19 +8,24 @@ app.controller("mainCtrl", function ($scope, $timeout, $window, $location) {
         {
             city: "Vancouver",
             groups: [
-                {id: 0, name: "Vancouver Rental - Brazil", url: "https://chat.whatsapp.com/IFxqCtwmLwqGJwb3YyPXVp"},
-                {id: 1, name: "Vancouver Rental - India", url: "https://chat.whatsapp.com/HKdBBsPri8fJLifqjmVral"},
-                {id: 2, name: "Vancouver Rental - China", url: "https://chat.whatsapp.com/BzPQWHc8miTGdF7oxqlOLy"},
-                {id: 3, name: "Vancouver Rental - Philippines", url: "https://chat.whatsapp.com/BPaSeqj5cR0FSvDUVC5ge5"}
+                {
+                    id: 0,
+                    name: "Vancouver Rental - General",
+                    url: "https://chat.whatsapp.com/FPiYNYl4IGuEn3hjsAbbWR"
+                }, {id: 1, name: "Vancouver Rental - Brazil", url: "https://chat.whatsapp.com/IFxqCtwmLwqGJwb3YyPXVp"},
+                {id: 2, name: "Vancouver Rental - India", url: "https://chat.whatsapp.com/HKdBBsPri8fJLifqjmVral"},
+                {id: 3, name: "Vancouver Rental - China", url: "https://chat.whatsapp.com/BzPQWHc8miTGdF7oxqlOLy"},
+                {id: 4, name: "Vancouver Rental - Philippines", url: "https://chat.whatsapp.com/BPaSeqj5cR0FSvDUVC5ge5"}
             ]
         },
         {
             city: "Toronto",
             groups: [
-                {id: 0, name: "Toronto Rental - Brazil", url: "https://chat.whatsapp.com/DfLBBgUAkh0EBLGKbORdpQ"},
-                {id: 1, name: "Toronto Rental - India", url: "https://chat.whatsapp.com/JNoUPDcI03pKk6E3gqwqLs"},
-                {id: 2, name: "Toronto Rental - China", url: "https://chat.whatsapp.com/KTSPjPKtZub5ZbhDVPUiTI"},
-                {id: 3, name: "Toronto Rental - Philippines", url: "https://chat.whatsapp.com/KKWfxQEWmirBo4tQp0XM7A"}
+                {id: 0, name: "Toronto Rental - General", url: "https://chat.whatsapp.com/HLQgB6g7ckt7buE1QCqYsA"},
+                {id: 1, name: "Toronto Rental - Brazil", url: "https://chat.whatsapp.com/DfLBBgUAkh0EBLGKbORdpQ"},
+                {id: 2, name: "Toronto Rental - India", url: "https://chat.whatsapp.com/JNoUPDcI03pKk6E3gqwqLs"},
+                {id: 3, name: "Toronto Rental - China", url: "https://chat.whatsapp.com/KTSPjPKtZub5ZbhDVPUiTI"},
+                {id: 4, name: "Toronto Rental - Philippines", url: "https://chat.whatsapp.com/KKWfxQEWmirBo4tQp0XM7A"}
             ]
         }
 
@@ -36,7 +41,7 @@ app.controller("mainCtrl", function ($scope, $timeout, $window, $location) {
     $scope.selectedGroups = $scope.groups[0].groups; //default vancouver
 
     $scope.user = {
-        group: $scope.selectedGroups[1], //vancouver
+        group: $scope.selectedGroups[0], //vancouver
         city: $scope.cities[0], //vancouver
         email: "",
     };
@@ -47,6 +52,7 @@ app.controller("mainCtrl", function ($scope, $timeout, $window, $location) {
 
     /* Connect to firebase =========================================== */
 // Initialize Firebase
+    // Initialize Firebase
     var config = {
         apiKey: "AIzaSyB3iOzD2pUfXtMkeQtl8H6_B1DJ8raD8TI",
         authDomain: "rentalmoose-46113.firebaseapp.com",
